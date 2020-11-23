297f668b8bc1b62f070b6f932d45ac094ca3e569
eduardofbrum <46854441+eduardofbrum@users.noreply.github.com>
2020-11-23 20:58:28 -0300

    Send JSON with the app colors

--- app.js
+++ app.js
@@ -3,9 +3,14 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Teste heroku automatico');
+  res.json({ 
+    textPrimary: "#FFFFFF",
+    elementBackground: "#2E2D33",
+    primaryRed: "#F05454",
+    primaryBlue: "#5FDDE5",
+    primaryOrange: "#EE6F57",
+    primaryGreen: "#50D890" })
 });
 
 app.listen(PORT, function () {
-  console.log('Example app listening on port 3000!');
 });
